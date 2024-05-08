c38d9327f22d0b6cabf52f5be9e09b19ca9e0cdc
guasam <guasamos@outlook.com>
2024-05-09 03:09:11 +0530

    Hello dirty fellow!

--- scripts/Example.cs
+++ scripts/Example.cs
@@ -1,12 +1,11 @@
 using Godot;
-using System;
 
 public partial class Example : Sprite2D
 {
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		GD.Print("Aoaah");
+		GD.Print("Hello Dirty Fellow!");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
